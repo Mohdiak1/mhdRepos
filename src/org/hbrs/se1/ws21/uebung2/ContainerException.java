0643a5e783b3989ee38ac745bbe5d517c14936e4
Mohdiak1 <mohamadou.diakite@smail.inf.h-brs.de>
2021-10-19 20:48:02 +0200

    Hinzufügen und Löschen eines Member in Container

---
+++ src/org/hbrs/se1/ws21/uebung2/ContainerException.java
@@ -0,0 +1,10 @@
+package org.hbrs.se1.ws21.uebung2;
+
+public class ContainerException extends Exception{
+    //kons standard
+    public ContainerException() {};
+    //Kons
+    public ContainerException(String msg) {
+        super(msg);
+    }
+}
